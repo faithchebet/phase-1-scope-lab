bce9522b9a1a23f95909ed0376212a11ff4167ef
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-15 21:36:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName;
+
+function declareBestCustomer() {
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+customerName = declareBestCustomer();
+console.log(customerName); 
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'bob';
+    return bestCustomer;
+}
+
+customerName = overwriteBestCustomer();
+console.log(customerName); 
+
+const leastFavoriteCustomer = 'charlie';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
